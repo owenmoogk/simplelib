357b4000e8e0a3e9662d8ee7d1903c0c32d0c6f9
owenmoogk <owenmoogk@gmail.com>
2021-01-22 23:14:16 -0500

    moved testing into file

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -134,3 +134,50 @@ class singlyLinkedList:
             nextNode = currentNode.next
             del(currentNode.data) 
             currentNode = nextNode
+
+if __name__ == "__main__":
+    mylist = singlyLinkedList()
+
+    mylist.display()
+    mylist.append(1)
+    mylist.append(6)
+    mylist.display()
+    print(mylist.size())
+    mylist.append("boiiiii smd")
+    print(mylist.size())
+    mylist.display()
+    print(mylist.get(1))
+    mylist.erase(1)
+    mylist.display()
+
+    mylist.insertAtIndex("hello",1)
+    mylist.display()
+
+    mylist.insertBefore(545,"hello")
+    mylist.insertAfter(545,"hello")
+    mylist.insertAfter(545,"boiiiii smd")
+    mylist.display()
+    print(mylist.size())
+    mylist.insertAtIndex("hello",5)
+    mylist.display()
+
+    mylist.eraseItem(545)
+    mylist.eraseItem(545)
+    mylist.eraseItem(545)
+    mylist.display()
+
+    print(mylist.inList(1))
+    print(mylist.inList(3))
+
+    print(mylist.getItemIndex("boiiiii smd"))
+    print(mylist.getItemIndex("hello"))
+    print(mylist.getItemIndex(545))
+
+    mylist.append(1)
+    mylist.append(6)
+    mylist.display()
+    mylist.erase(1)
+    mylist.insertAfter("hello",1)
+    mylist.display()
+    print("Size:",mylist.size())
+    print("'hello' index:",mylist.getItemIndex("hello"))
