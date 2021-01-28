59fc5e7a54132e03e138ca354cd2c5b5afae6c37
owenmoogk <owenmoogk@gmail.com>
2021-01-27 23:26:55 -0500

    added option for multiple indexes to be returned

--- algorithms/binary-search.py
+++ algorithms/binary-search.py
@@ -17,26 +17,55 @@ def binarySearch(numbers, numToFind):
             rightIndex = midIndex - 1
     return(-1)
 
-def binarySearchRecursive(numbersList, numToFind, leftIndex = 0, rightIndex = -1):
-    if rightIndex == -1:
-        rightIndex = len(numbersList) - 1
-    if rightIndex < leftIndex:
-        return(-1)
+def binarySearchMultiple(numbers, numToFind):
+    leftIndex = 0
+    rightIndex = len(numbers)
+    foundIndex = None
+
+    while leftIndex <= rightIndex:
+        midIndex = (leftIndex + rightIndex) // 2
+        midNumber = numbers[midIndex]
+        
+        if midNumber == numToFind:
+            foundIndex = midIndex
+            break
+
+        if midNumber < numToFind:
+            leftIndex = midIndex + 1
+        elif midNumber > numToFind:
+            rightIndex = midIndex - 1
     
-    midIndex = (leftIndex + rightIndex) //2
-    midNumber = numbersList[midIndex]
+    if foundIndex:
+        allIndexes = []
+        index = foundIndex
+        while numbers[index] == numToFind:
+            allIndexes.append(index)
+            index -= 1
+        index = foundIndex + 1
+        while numbers[index] == numToFind:
+            allIndexes.append(index)
+            index += 1
+        allIndexes.sort()
+        return(allIndexes)
+    return(-1)
 
-    if midNumber == numToFind:
-        return(midIndex)
-    if midNumber > numToFind:
-        leftIndex = midIndex + 1
-    else:
-        rightIndex = midIndex - 1
-
-    return(binarySearchRecursive(numbersList, numToFind, leftIndex, rightIndex))
+# def binarySearchRecursive(numbersList, numToFind, leftIndex, rightIndex):
+#     if rightIndex < leftIndex: # break case
+#         return(-1)
+#     midIndex = (leftIndex + rightIndex) // 2
+#     if midIndex >= len(numbersList) or midIndex < 0: # edge case
+#         return(-1)
+#     midNumber = numbersList[midIndex]
+#     if midNumber == numToFind:
+#         return(midIndex)
+#     if midNumber < numToFind:
+#         leftIndex = midIndex + 1
+#     else:
+#         rightIndex = midIndex - 1
+#     return(binarySearchRecursive(numbersList, numToFind, leftIndex, rightIndex))
 
 if __name__ == "__main__":
-    numbers = [1,5,6,7,8,9,10,15,3215]
-    num = 7
+    numbers = [1,5,6,7,8,8,8,8,9,10,15,3215]
+    num = 8
 
-    print(binarySearchRecursive(numbers, num))
+    print(binarySearchMultiple(numbers, num))
